ca3df3470be2bc1b81553bd446dc0f83549e0096
Chris Bywater <chrisb@dataplan.co.uk>
2018-05-11 10:37:27 +0100

    use tmuxp for session switching instead of choose-tree

--- tmux/scripts/tmuxp_menu.sh
+++ tmux/scripts/tmuxp_menu.sh
@@ -2,5 +2,4 @@
 
 window=$(tmux new-window -d -P -F "#{window_id}")
 tmux send-keys -t $window "selection=\$(ls ~/.tmuxp | sed -e 's:\.[^./]*$::' | fzf +m) && tmuxp load \$selection -y && tmux kill-window" C-m
-sleep 0.1
 tmux select-window -t $window
