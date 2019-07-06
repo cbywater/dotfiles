44811cda5271d80be106f48065e3140a2afd06df
Chris Bywater <chrisb@dataplan.co.uk>
2019-07-06 09:37:35 +0100

    don't need that bit anymore

--- tmux/scripts/tmuxp_menu.sh
+++ tmux/scripts/tmuxp_menu.sh
@@ -3,7 +3,7 @@
 window=$(tmux new-window -d -P -F "#{window_id}")
 tmux send-keys -t $window \
     "selection=\$( \
-        ls ~/dotfiles/launch_scripts | sed -e 's:\.[^./]*$::' | fzf +m --preview='~/dotfiles/launch_scripts/{}/preview.sh' \
+        ls ~/dotfiles/launch_scripts | fzf +m --preview='~/dotfiles/launch_scripts/{}/preview.sh' \
     ) \
     && ~/dotfiles/launch_scripts/\$selection/launch.sh \
     && tmux kill-window" \
