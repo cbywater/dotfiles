6299e7511fdf94175674a964896dd8302d8be31e
Chris Bywater <chrisbywater@gmail.com>
2019-07-06 11:30:23 +0100

    move launch scripts search directory out of this repo

--- tmux/scripts/tmuxp_menu.sh
+++ tmux/scripts/tmuxp_menu.sh
@@ -3,9 +3,9 @@
 window=$(tmux new-window -d -P -F "#{window_id}")
 tmux send-keys -t $window \
     "selection=\$( \
-        ls ~/dotfiles/launch_scripts | fzf +m --preview='~/dotfiles/launch_scripts/{}/preview.sh' \
+        ls ~/.launch_scripts | fzf +m --preview='~/.launch_scripts/{}/preview.sh' \
     ) \
-    && ~/dotfiles/launch_scripts/\$selection/launch.sh \
+    && ~/.launch_scripts/\$selection/launch.sh \
     && tmux kill-window" \
     C-m
 
